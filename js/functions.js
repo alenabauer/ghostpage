be9387b2359a596cff6bf13b64dcd89b94b37022
Alena Bauer <alena.a.bauer@gmail.com>
2021-10-29 15:56:59 +0200

    add movement on y axis

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,7 @@
 const ghost = document.querySelector(".ghost");
 console.log(ghost);
 let x = 0;
+let y = 0;
 
 // const moveGhost = () => {
 //   console.log(ghost.style.left);
@@ -11,10 +12,15 @@ let x = 0;
 setInterval(() => {
   // console.log(ghost.style.left);
   ghost.style.left = `${x}px`;
+  ghost.style.top = `${y}px`;
   if (window.screen.width < x) {
     x = 0;
   };
+  if (window.screen.height < y) {
+    y = 0;
+  }
   x++;
+  y++;
 }, 7);
 
 // console.log("Nothing is working!");
