de350d6177bad6aa455e126244fd6228c56d2741
Alena Bauer <alena.a.bauer@gmail.com>
2021-10-29 16:57:49 +0200

    annoying ghost

--- js/functions.js
+++ js/functions.js
@@ -1,7 +1,7 @@
 const ghost = document.querySelector(".ghost");
 console.log(ghost);
-let x = 0;
-let y = 0;
+let x = Math.random() * 1000;
+let y = Math.random() * 1000;
 
 // const moveGhost = () => {
 //   console.log(ghost.style.left);
@@ -13,15 +13,17 @@ setInterval(() => {
   // console.log(ghost.style.left);
   ghost.style.left = `${x}px`;
   ghost.style.top = `${y}px`;
-  if (window.screen.width < x) {
+  if (window.screen.width < x || x < 0) {
     x = 0;
   };
-  if (window.screen.height < y) {
+  if (window.screen.height < y || y < 0) {
     y = 0;
-  }
-  x++;
-  y++;
-}, 7);
+  };
+  var rand1 = Math.random() < 0.5 ? -30 : 30;
+  var rand2 = Math.random() < 0.5 ? -30 : 30;
+  x += rand1;
+  y += rand2;
+}, 3);
 
 // console.log("Nothing is working!");
 // window.requestAnimationFrame(moveGhost);
